93b90bbd86b6f999bc63df92b177277844159697
d-jackson-lum <d.jackson@lumicks.com>
2025-04-18 16:53:32 +0200

    FFT fitting and improved user input

--- Load_plot_h5s.py
+++ Load_plot_h5s.py
@@ -6,89 +6,103 @@ Created on Mon Apr 14 17:03:03 2025
 """
 
 
-""" User Inputs """
+"""
+User Inputs
+"""
+#Choosing the desired file
+folder_path = 'C:/Users/DanielJackson/Documents/Service/BA003_HQ/25.02.19_PI_Nanostage_Tests/Stuck_bead/'
+#'C:/Users/DanielJackson/Downloads/'
+file_name = 'Stuck_bead_2nd_min_378fps.h5'
+#'Marker_during_phone_recording.h5'
 
-Data_Channel = 'Force HF'
-start_time = 0
-end_time = 10
+#Choosing the desired channel and setting the time range to be plotted
+Data_Channel = 'Force HF' #Will proccess all data associated with this channel #e.g. Force 1x, Force 2x, etc.
+start_time = 2
+end_time = 3
+
+#Settings used for plotting (applies only to raw data output, not FFT)
+Xaxis = 'Time (s)' 
+Yaxis = 'Force (pN)' 
+X_scale = 'linear' 
+Y_scale = 'linear' 
+
+#Choose whether to calculate and plot the FFT
+Do_FFT = 1 # "1" to calculate the FFT. "0" does not calculate it.
+#If Do_FFT is "0" then plotting and fitting will not happen
+Plot_FFT = 1 # "1" to also plot the FFT of the data. "0" to only plot data
+Fit_FFT = 1 # "1" to include a fit to the FFT. "0" to not include 
+downsampling_number = 1
+
+#Choose to export data. Raw data as csv or txt file, and the png of plot
+export = 'no' #options are 'yes' or 'no'
+export_type = 'txt'  #Options are 'txt' or 'csv' #Defualts to 'txt' for incorrect values
+Destination_folder = 'C:/Users/DanielJackson/Documents/h5_saving/'
+
+"""
+End of User Inputs
+"""
+
 
 """
 Import Modules
 """
-
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import rfft, rfftfreq # fft, fftfreq ## fft and rfft do the same thing, but rfft eliminates symmettry around 0
 import lumicks.pylake as lk
-import sys
+from h5_functions import Get_Slices, Get_FFT, fit_analytical_lorentzian, Prepare_export#, lorentzian
+from lumicks.pylake.force_calibration.power_spectrum import PowerSpectrum
+import pandas as pd
+#import numpy as np
+#from scipy.fft import rfft, rfftfreq # fft, fftfreq ## fft and rfft do the same thing, but rfft eliminates symmettry around 0
+#import sys
 #from h5_options import Data_Slice
 #import h5py
-#import pandas as pd
 
 """
 Load the file
 """
-folder_path = 'C:/Users/DanielJackson/Downloads/'
-file_name = 'Marker_during_phone_recording.h5'
-file_ID = file_name.split('.')[0]
 
+file_ID = file_name.split('.')[0]
 file = lk.File(folder_path + file_name)
 #print(file)
 
 """
 Choose the data column to load
--calls a list of all options in h5 files. Will plot all the choices for each data type
--saves time in plotting multiple axes at the same time
+Finds all slices present for the chosen data channel
+Will proccess all data associated with this channel #e.g. Force HF: Force 1x, Force 2x, etc.
 """
 
-
-
-Channels = sorted(file.h5.items())
-channel = ''
-channels = ''
-for channel, slices in Channels:
-    channels += channel + ', '
-    data_slices = []
-    if Data_Channel == channel:
-        for sli in slices:
-            data_slices.append(sli)
-            checker = 1
-        break
-    else:
-        checker = 0
-        
-if checker == 0:
-    sys.exit('No matching channels in this file. Try again using the following options: ' + channels)
-
-#Slices = Data_Slice(Data_Channel)
-Slices = data_slices
+Slices = Get_Slices(file, Data_Channel)
 
 for s in Slices:
     
     Data_slice = s
     
     data = file[Data_Channel][Data_slice].data
-    points = file[Data_Channel][Data_slice].timestamps
-    time_ns = (points-points[0])
+    timestamp = file[Data_Channel][Data_slice].timestamps
+    time_ns = (timestamp - timestamp[0])
     time_s = time_ns*1e-9
     Nd = len(data)
     
     """
-    Perform transform on the data
+    Check for empty slice -> skip if empty
+    """
+    if Nd < 2:
+        continue
+    
+    """
+    Perform fourier transform on the data
     """
     
-    Sample_rate = 1/(time_s[1] - time_s[0]) #Hz
-    datafft = rfft(data)
-    abs_datafft = np.abs(datafft)
-    freq = rfftfreq(Nd, 1/Sample_rate) #(Number of points, sample rate)
+    data_fft, freq_fft, Sample_rate = Get_FFT(data, time_s[0], time_s[1]) #outputs (FFT_of_data, Frequencies_of_data, Sample_rate)
     
     """
     Plotting region
     """
-
+    #start and end points of the data being analyzed
     start_pnt = start_time * Sample_rate
     end_pnt = end_time * Sample_rate
     
+    #some fail safes to prevent errors
     if start_pnt < 0:
         start_pnt = 0
     if end_pnt > (Nd - 1):
@@ -96,30 +110,106 @@ for s in Slices:
     if start_pnt >= end_pnt:
         start_pnt = end_pnt - 1
         
+    #Creat array of the desired time window    
     start_plt = int(start_pnt)
     end_plt = int(end_pnt)
     data_plt = data[start_plt:end_plt]
     time_plt = time_s[start_plt:end_plt]
     
+    ps_data = PowerSpectrum(data_plt, Sample_rate)
+    ps_data = ps_data.downsampled_by(downsampling_number)
+
+    """
+    Exporting parameters
+    """
+    #Set folder -> control at the top of page
+    Save_as = Destination_folder + Data_Channel + ' ' + s
+    #Convert yes/no to binary control for exporting
+    if export == 'yes':
+        do_export = 1
+    else:
+        do_export = 0
     
+    #Set type to txt or csv  
+    if export_type != 'txt' and export_type != 'csv':
+        print('Improper export type set. Defaulting to .txt file type.')
+        export_type = 'txt'
+
     """
     Plot the data
     """
+    plot_title = Data_Channel + ' ' + s
+    
+    X_plt = time_plt
+    Y_plt = data_plt
     
     plt.figure()
-    plt.plot(time_plt, data_plt)
-    plt.xlabel('Time (s)')
-    plt.ylabel('y axis')
-    plot_title = Data_Channel + ' ' + s
+    plt.plot(X_plt, Y_plt) #, label='Data', color='blue')
+    plt.xlabel(Xaxis)
+    plt.ylabel(Yaxis)
+    plt.xscale(X_scale)
+    plt.yscale(Y_scale)
     plt.title(plot_title)
+    if do_export: plt.savefig(Save_as + ' plot.png')
     plt.show()
     
-    #plt.figure()
-    #plt.plot(freq, abs_datafft)
-    #plt.xscale('log')
-    #plt.yscale('log')
-    #plt.xlabel('Time (s)')
-    #plt.ylabel('Position (um)')
-    #plt.title('Transform of Trap 2 position')
-    #plt.show()
+    if do_export:
+        framed_data = Prepare_export(X_plt,Y_plt,Xaxis,Yaxis)
+        framed_data.to_csv(Save_as + '.' + export_type)
+    
+    """
+    Calculating the FFT and fit
+    """
+    #These are pylake functions. makes data into a power spectrum object that
+    #gets passed through the pylake package
+    if Do_FFT:
+        
+        data_freq = ps_data.frequency
+        data_amplitude = ps_data.power
+        if Fit_FFT:    
+            Fit_Results = fit_analytical_lorentzian(ps_data)
+        
+            fc = Fit_Results[0]
+            D = Fit_Results[1]
+            sigma_fc = Fit_Results[2]
+            sigma_D = Fit_Results[3]
+            ps_fit = Fit_Results[4]
+
+            fit_freq = ps_fit.frequency
+            fit_amplitude = ps_fit.power
+    
+        if Plot_FFT:
+            plt.figure#(figsize=(8, 6))
+            #Ignore very first data point in plotting FFT. Keeps giving  a miniscule value (e.g. 1e-41)
+            plt.plot(data_freq[1:], data_amplitude[1:], label='Data', color='blue')
+            if Fit_FFT: plt.plot(fit_freq[1:], fit_amplitude[1:], label='Lorentzian Fit', color='red')
+            plt.xlabel('Frequencies')
+            plt.ylabel('Amplitudes (V**2/Hz)')
+            plt.xscale('log')
+            plt.yscale('log')
+            plt.legend()
+            if do_export: plt.savefig(Save_as + ' FFT plot.png')
+            plt.title('Lorentzian Fit to ' + plot_title)
+            plt.show()
+            
+            if do_export:
+                data_freq = ps_data.frequency
+                data_amplitude = ps_data.power
+                fit_freq = ps_fit.frequency
+                fit_amplitude = ps_fit.power
+                framed_data = Prepare_export(data_freq,data_amplitude,'Frequencies','Amplitudes')
+                if Fit_FFT:
+                    framed_fit = Prepare_export(fit_freq,fit_amplitude,'Fit Frequencies','Fit Amplitudes')
+                    FFT_with_fit = pd.concat([framed_data,framed_fit])
+                    FFT_with_fit.to_csv(Save_as + ' FFT with fit.' + export_type)
+                else:
+                    framed_data.to_csv(Save_as + ' FFT.' + export_type)
+
+print('Analysis completed.')
+
+
+
+
+
+
 
---
+++ h5_functions.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 18 10:12:52 2025
+
+@author: DanielJackson
+"""
+
+import numpy as np
+from scipy.fft import rfft, rfftfreq # fft, fftfreq ## fft and rfft do the same thing, but rfft eliminates symmettry around 0
+import sys
+from collections import namedtuple
+import math
+from itertools import product
+import pandas as pd
+#from lumicks.pylake.force_calibration.detail.power_models import (
+#    ScaledModel, fit_analytical_lorentzian,)
+#import lumicks.pylake as lk
+
+
+def Get_Slices(file, Data_Channel):
+    Channels = sorted(file.h5.items())
+    channel = ''
+    channels = ''
+    for channel, slices in Channels:
+        channels += channel + ', '
+        data_slices = []
+        if Data_Channel == channel:
+            for sli in slices:
+                data_slices.append(sli)
+                checker = 1
+            break
+        else:
+            checker = 0
+            
+    if checker == 0:
+        sys.exit('No matching channels in this file. Try again using the following data channel options: ' + channels)
+    
+    return data_slices
+
+
+
+def Get_FFT(data, time_0, time_1):
+    
+    Nd = len(data)
+    Sample_rate = 1/(time_1 - time_0) #Hz
+    FFT_of_data = rfft(data)
+    absolute_value_FFT = np.abs(FFT_of_data)
+    Frequencies_of_FFT = rfftfreq(Nd, 1/Sample_rate) #(Number of points, sample rate)
+    
+    return absolute_value_FFT, Frequencies_of_FFT, Sample_rate
+
+
+def Basic_Lorentzian(D, f_c, f):
+    return (D/(2*(np.pi**2)))/(f_c**2 + f**2)
+
+
+def fit_analytical_lorentzian(ps):
+    """Performs an analytical least-squares fit of a Lorentzian Power Spectrum.
+
+    Based on Section IV from ref. 1. Note that the equations for the statistics Spq are divided
+    by a factor of two since we defined the power spectrum in V^2/Hz instead of 0.5 V^2/Hz.
+
+    Parameters
+    ----------
+    ps : PowerSpectrum
+        Power spectrum data. Should generally be block-averaged, before passing
+        into this function.
+
+    Returns
+    -------
+    namedtuple (fc, D, sigma_fc, sigma_D, ps_fit)
+        Attributes:
+        - `fc` : corner frequency [Hz]
+        - `D`: diffusion constant [V^2/s]
+        - `sigma_fc`, `sigma_D`: 1-sigma confidence intervals for `fc` and `D`
+        - `ps_fit`: :class:`~lumicks.pylake.force_calibration.power_spectrum.PowerSpectrum` object with model fit
+
+    """
+    FitResults = namedtuple(
+        "AnalyticalLorentzianFitResults", ["fc", "D", "sigma_fc", "sigma_D", "ps_fit"]
+    )
+
+    # Calculate S[p,q] elements (Ref. 1, Eq. 13-14).
+    Spq = np.zeros((3, 3))
+    for p, q in product(range(3), range(3)):
+        Spq[p, q] = np.sum(np.power(ps.frequency, 2 * p) * np.power(ps.power, q))
+
+    # Calculate a and b parameters (Ref. 1, Eq. 13-14).
+    a = (Spq[0, 1] * Spq[2, 2] - Spq[1, 1] * Spq[1, 2]) / (
+        Spq[0, 2] * Spq[2, 2] - Spq[1, 2] * Spq[1, 2]
+    )
+    b = (Spq[1, 1] * Spq[0, 2] - Spq[0, 1] * Spq[1, 2]) / (
+        Spq[0, 2] * Spq[2, 2] - Spq[1, 2] * Spq[1, 2]
+    )
+
+    # Having a and b, calculating fc and D is trivial.
+    a_div_b = a / b
+    if a_div_b > 0:
+        fc = math.sqrt(a_div_b)  # corner frequency [Hz]
+    else:
+        # When the corner frequency is very low and the power spectrum doesn't reach all the way,
+        # this can fail. As initial guess we then use the half the lowest nonzero frequency observed
+        # in the power spectrum (optimal when assuming uniform prior for our guess). Note that zero
+        # isn't a valid choice, since this leads to nan's and infinities down the road.
+        fc = 0.5 * (ps.frequency[0] if ps.frequency[0] > 0 else ps.frequency[1])
+
+    if b > 0:
+        D = (1 / b) * (math.pi**2)  # diffusion constant [V^2/s]
+    else:
+        # If b <= 0, the analytic estimation procedure failed. Using a negative value for the
+        # diffusion would place the initial guess outside the feasible physical parameter range.
+        # This would result in a failing non-linear fit. In this case, we need a different way of
+        # estimating this quantity. The power spectral density at frequency zero is given by:
+        #
+        #   P0 = D / (pi**2 * fc**2)
+        #
+        # Therefore, an alternative method to get a rough estimate for this quantity would be:
+        #
+        #   D_guess = pi**2 * fc**2 * power[0]
+        #
+        # Where power[0] is the lowest frequency in the spectrum.
+        D = math.pi**2 * fc**2 * ps.power[0]
+
+    # Fitted power spectrum values.
+    ps_fit = ps.with_spectrum(1 / (a + b * np.power(ps.frequency, 2)))
+
+    # Error propagation (Ref. 1, Eq. 25-28).
+    x_min = ps.frequency.min() / fc
+    x_max = ps.frequency.max() / fc
+
+    u = (
+        (2 * x_max) / (1 + x_max**2)
+        - (2 * x_min) / (1 + x_min**2)
+        + 2 * math.atan((x_max - x_min) / (1 + x_min * x_max))
+    )
+    v = (4 / (x_max - x_min)) * (math.atan((x_max - x_min) / (1 + x_min * x_max))) ** 2
+    s_fc = math.sqrt(math.pi / (u - v))
+    sigma_fc = fc * s_fc / math.sqrt(math.pi * fc * ps.total_duration)
+
+    s_D = math.sqrt(u / ((1 + math.pi / 2) * (x_max - x_min))) * s_fc
+    sigma_D = D * math.sqrt((1 + math.pi / 2) / (math.pi * fc * ps.total_duration)) * s_D
+
+    return FitResults(fc, D, sigma_fc, sigma_D, ps_fit)
+
+
+def Prepare_export(Xdata,Ydata,Xaxis_label,Yaxis_label):
+    raw_data = np.array([Xdata,Ydata])
+    atad_war = np.transpose(raw_data)
+    framed_data = pd.DataFrame(atad_war, columns=[Xaxis_label, Yaxis_label])
+    framed_data = framed_data.drop(framed_data.index[0])
+    return framed_data
+    
+
+
+
+
