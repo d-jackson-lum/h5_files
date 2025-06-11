2b3ce3370eaa4ee5d0ed5d33c22b2a6b0a076bdf
d-jackson-lum <d.jackson@lumicks.com>
2025-06-11 15:14:47 +0200

    Histogram and fitting
    generate a histogram of data and automatically plot it on the right side of the data

--- Load_plot_h5s.py
+++ Load_plot_h5s.py
@@ -17,15 +17,10 @@ file_name = 'Stuck_bead_2nd_min_378fps.h5'
 
 #Choosing the desired channel and setting the time range to be plotted
 #Data_Channel = 'Force HF' #Will proccess all data associated with this channel #e.g. Force 1x, Force 2x, etc.
-
-start_time = 2
-end_time = 3
-
-#Settings used for plotting (applies only to raw data output, not FFT)
-Xaxis = 'Time (s)' 
-Yaxis = 'Force (pN)' 
-X_scale = 'linear' 
-Y_scale = 'linear' 
+global Start_value
+global End_value
+start_time = int(Start_value)
+end_time = int(End_value)
 
 #Choose whether to calculate and plot the FFT
 Do_FFT = 1 # "1" to calculate the FFT. "0" does not calculate it.
@@ -49,6 +44,7 @@ End of User Inputs
 """
 Import Modules
 """
+
 import matplotlib.pyplot as plt
 import scipy as sp
 import lumicks.pylake as lk
@@ -56,7 +52,15 @@ from h5_functions import Get_Slices, Get_FFT, fit_analytical_lorentzian, Prepare
 from lumicks.pylake.force_calibration.power_spectrum import PowerSpectrum
 import pandas as pd
 import sys
-#import numpy as np
+from scipy.optimize import curve_fit
+import numpy as np
+import math
+#matplotlib inline
+width = 8
+height = 4.5
+resolution_dpi = 200
+plt.rcParams['figure.figsize'] = [width, height]
+plt.rcParams['figure.dpi'] = resolution_dpi
 #from scipy.fft import rfft, rfftfreq # fft, fftfreq ## fft and rfft do the same thing, but rfft eliminates symmettry around 0
 #import sys
 #from h5_options import Data_Slice
@@ -64,6 +68,8 @@ import sys
 
 #global global_test
 #print(global_test)
+
+
 
 """
 Load the file
@@ -78,6 +84,10 @@ Choose the data column to load
 Finds all slices present for the chosen data channel
 Will proccess all data associated with this channel #e.g. Force HF: Force 1x, Force 2x, etc.
 """
+
+print(' ')
+print('------------------------')
+print(' ')
 
 global Data_Channel
 Slices, Channels, checker = Get_Slices(file, Data_Channel)
@@ -96,6 +106,7 @@ else:
 for s in Slices:
     
     Data_slice = s
+    #print(file)
     
     data = file[Data_Channel][Data_slice].data
     timestamp = file[Data_Channel][Data_slice].timestamps
@@ -158,6 +169,21 @@ for s in Slices:
     """
     Plot the data
     """
+    #Settings used for plotting (applies only to raw data output, not FFT)
+    Xaxis = 'Time (s)' 
+    Yaxis = ''
+    if(Data_Channel=='Force LF' or Data_Channel=='Force HF'):
+        Yaxis = 'Force (pN)'
+    elif(Data_Channel=='Bead position' or Data_Channel=='Nanostage position'):
+        Yaxis = 'Position (um)'
+    elif(Data_Channel=='Trap position' or Data_Channel=='1+2 position'):
+        Yaxis = 'Position (um)'
+    else:
+        Yaxis = 'Units'
+    
+    X_scale = 'linear' 
+    Y_scale = 'linear'
+    
     plot_title = Data_Channel + ' ' + s
     
     X_plt = time_plt
@@ -167,24 +193,88 @@ for s in Slices:
     bin_width = 2 * IQR / (Nd**(1/3)) ##based on Freedman-Diaconis rule for data spread
     start_bin = min(Y_plt)
     end_bin = max(Y_plt)
-    histing_offset_percent = 0.25
+    histing_offset_percent = 0.3
     histing_bins = int(abs(((end_bin - start_bin)/bin_width)) * histing_offset_percent)
+    print(histing_bins)
     
-    fig = plt.figure()
-    gs = fig.add_gridspec(1, 2, hspace=0, wspace=0.01, width_ratios=[2,1])
+    if histing_bins > 100:
+        histing_bins = int(histing_bins/2)
+
+    fig = plt.figure(num=0)
+    gs = fig.add_gridspec(1, 2, hspace=0, wspace=0.005, width_ratios=[2,1])
     (raw, histo) = gs.subplots(sharey=True)
-    fig.suptitle('Raw data with histogram')
+    fig.suptitle(plot_title)#('Raw data with histogram')
     raw.plot(X_plt, Y_plt, label='Data')
     raw.set(xlabel=Xaxis, ylabel=Yaxis)
-    histo.hist(Y_plt, orientation='horizontal', bins = histing_bins, label='Histogram', color='C7')
+    #histed, binned = np.histogram(Y_plt, bins = histing_bins)
+    
+    histed, binned, _ = histo.hist(Y_plt, orientation='horizontal', bins = histing_bins, label='Histogram', color='C7')
     histo.set(xlabel='Amplitude')
     histo.axes.get_xaxis().set_visible(False)
     histo.axes.get_yaxis().set_visible(False)
     histo.axis('off')
+    #Fit the Histrogram
+    def gauss(x, H, A, x0, sigma):
+        import numpy as np
+        return H + A * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
+    
+    bin_centers=[]
+    i=0
+    for histt in histed:
+        bin1 = binned[i]
+        bin2 = binned[i+1]
+        bin_center = (bin1 + bin2)/2
+        bin_centers.append(bin_center)
+        i += 1
+    guess_H = 0
+    guess_A = max(histed)
+    Guess_x0 = bin_centers[int(len(bin_centers)/2)]
+    Guess_sigma = bin_centers[int(len(bin_centers)/1.5)] - bin_centers[int(len(bin_centers)/2)]
+    #print(guess_H, guess_A, Guess_x0, Guess_sigma)
+    guess = [guess_H, guess_A, Guess_x0, Guess_sigma]
+    params, something = curve_fit(gauss, bin_centers, histed, guess)
+    fit_H = params[0]
+    fit_A = params[1]
+    fit_x0 = params[2]
+    fit_sigma = params[3]
+    FWHM = 2.35*fit_sigma
+    fit_histed = gauss(bin_centers, fit_H, fit_A, fit_x0, fit_sigma)
+    #plt.figure(num=1)
+    #plt.plot(bin_centers, histed, 'o', label='data')#, orientation='horizontal', label='Histogram', color='C7')
+    #plt.plot(bin_centers, fit_histed, '-', label='fit')
+    #plt.show()
+    histo.plot(fit_histed, bin_centers)#, color='r')
+    peak = 'Peak: ' + str('%s' % float('%.3g' % fit_x0))
+    FWHM_ = 'FWHM: ' + str('%s' % float('%.3g' % FWHM))
+    width_pixels = width * resolution_dpi
+    height_pixels = height * resolution_dpi
+    w_frac_1 = 0.67
+    w_frac_2 = 0.67
+    h_frac_1 = 0.12
+    h_frac_2 = 0.075
+    histo.annotate(peak, xy=(width_pixels*w_frac_1, height_pixels*h_frac_1), xycoords='figure pixels')
+    histo.annotate(FWHM_, xy=(width_pixels*w_frac_2, height_pixels*h_frac_2), xycoords='figure pixels')
+    histo.axes.set_xbound(lower = 0)
+    #histo.annotate(peak,
+    #        xy=(.95, .12), xycoords='figure fraction', fontsize=10,
+    #        horizontalalignment='right')
+    #histo.annotate(FWHM_,
+    #        xy=(.95, .075), xycoords='figure fraction', fontsize=10,
+    #        horizontalalignment='right')
     fig.show()
     
-    if do_export: plt.savefig(Save_as + ' plot.png')
+    #print(histed)
+    #print(binned)
+    #print('Peak position: ' + str(fit_x0))
+    #print(Guess_sigma)
+    #print(fit_sigma)
     plt.show()
+    
+    
+    
+    if do_export:
+        plt.savefig(Save_as + ' plot.png')
+
     
     if do_export:
         framed_data = Prepare_export(X_plt,Y_plt,Xaxis,Yaxis)
@@ -214,7 +304,7 @@ for s in Slices:
             fit_amplitude = ps_fit.power
     
         if Plot_FFT:
-            plt.figure#(figsize=(8, 6))
+            plt.figure(num=2)#(figsize=(8, 6))
             #Ignore very first data point in plotting FFT. Keeps giving  a miniscule value (e.g. 1e-41)
             plt.plot(data_freq[1:], data_amplitude[1:], label='Data', color='C7') #C7 is a gray color
             if Fit_FFT: plt.plot(fit_freq[1:], fit_amplitude[1:], label='Lorentzian Fit', color='C3') #C3 is a red color
--- Plotting_GUI_2.py
+++ Plotting_GUI_2.py
@@ -15,7 +15,7 @@ from h5_functions import Get_All_Slices, Get_Slices
 # file explorer window
 def browseFiles():
     global filepath
-    filepath = filedialog.askopenfilename(initialdir = "C:/Users/DanielJackson/Documents/Service/BA003_HQ/25.02.19_PI_Nanostage_Tests/Stuck_bead/",
+    filepath = filedialog.askopenfilename(initialdir = "C:/Users/DanielJackson/Desktop/",
 										title = "Select a File",
 										filetypes = (("h5 files", "*.h5*"), ("all files", "*.*")))
     # Change label contents
@@ -28,19 +28,19 @@ def browse_folder():
     return folder_path
 
 def get_file():
-    file_ID = filepath.split('.')[0]
     global file
-    file = lk.File(filepath)
     global Channels
+    file_ID = filepath.split('.')[0]
+    file = lk.File(filepath)
     Slices, Channels, checker = Get_Slices(file, "")
     channeled['values'] = Channels
     print("File loaded succesfully.")
     
 def pick_channel(event):
     global Data_Channel
+    global Slices
     Data_Channel = channeled.get()
     print(f"Selected channel: {Data_Channel}")
-    global Slices
     Slices, Channels, checker = Get_Slices(file, Data_Channel)
     Slices.append('All Slices')
     sliced['values'] = Slices
@@ -86,6 +86,28 @@ def check_check():
     global check_fit_val
     check_fft_val = fft_var.get()
     check_fit_val = fit_var.get()
+    
+def Set_start_time():
+    global Start_value
+    global End_value
+    Start_value = Start_spinbox.get()
+    End_value = End_spinbox.get()
+    print("Starting second for plot:", Start_value)
+    if(Start_value>=End_value):
+        start_plus_one = int(float(Start_value)+1)
+        End_value =  str(start_plus_one)
+        Set_end_time()
+
+def Set_end_time():
+    global End_value
+    global Start_value
+    End_value = End_spinbox.get()
+    Start_value = Start_spinbox.get()
+    if(Start_value>=End_value):
+        start_plus_one = int(float(Start_value)+1)
+        End_value =  str(start_plus_one) #f"{start_plus_one:.3f}"
+        #Set_end_time()
+    print("Ending second for plot:", End_value)
 																							
 # Create the root window
 window = Tk()
@@ -113,8 +135,7 @@ window.columnconfigure(4, weight=col_weight)
 
 # Create a File Explorer label
 label_file_explorer = Label(window,text = "Load and Plot h5 Files", height = 4, fg = "blue")#, width = 100)
-button_explore = Button(window, text = "Browse Files",command = browseFiles)  
-#button_loader = Button(window, text = "Load File", command = get_file)
+button_explore = Button(window, text = "Browse Files",command = browseFiles) 
 button_plotter = Button(window, text="Plot Data", command=plotting_script)
 
 label_channel = Label(window, text="Select data channel")
@@ -129,6 +150,19 @@ fit_var = BooleanVar()
 check_fft = ttk.Checkbutton(window, text='Plot FFT?', command=check_fft, variable=fft_var)
 check_fit = ttk.Checkbutton(window, text='Fit FFT?', command=check_fit, variable=fit_var)
 
+Start_var = StringVar(window)
+Start_var.set("0")
+Start_spinbox = ttk.Spinbox(window, from_=0, to=100, width=10, font=("Arial", 12), textvariable=Start_var, command=Set_start_time)
+Start_spinbox.config(state="normal", cursor="hand2", justify="center", wrap=False)
+
+End_var = StringVar(window)
+End_var.set("1")
+End_spinbox = ttk.Spinbox(window, from_=0, to=100, width=10, font=("Arial", 12), command=Set_end_time)
+End_spinbox.config(state="normal", cursor="hand2", justify="center", wrap=False, textvariable=End_var)
+
+Set_start_time()
+Set_end_time()
+
 label_file_explorer.grid(column = 0, row = 0, columnspan=5, sticky=EW)
 middle_col = 2
 button_explore.grid(column = middle_col, row = 1)
@@ -140,5 +174,8 @@ sliced.grid(column = middle_col,row = 5)
 button_plotter.grid(column = middle_col,row = 6)
 check_fft.grid(column = middle_col+1,row = 5)
 check_fit.grid(column = middle_col+1,row = 6)
+Start_spinbox.grid(column = middle_col-1,row = 5)
+End_spinbox.grid(column = middle_col-1,row = 6)
+
 # Let the window wait for any events
 window.mainloop()
